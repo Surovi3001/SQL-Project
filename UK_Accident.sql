4884830bf093447b0c8ff661a424c39ae15bb0ca
Surovi_M <59788708+Surovi3001@users.noreply.github.com>
2024-02-13 21:05:56 +0530

    Update UK_Accident.sql

--- UK_Accident.sql
+++ UK_Accident.sql
@@ -1,7 +1,1 @@
-select * from uk_accident;
 
-select day_of_week, sum(number_of_casualties) from uk_accident
-group by day_of_week;
-
-select day_of_week, max(speed_limit) as "Maximum Speed", min(speed_limit) as "Minimum Speed"
-from uk_accident group by day_of_week;
