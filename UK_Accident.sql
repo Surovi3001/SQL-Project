198bba50c630ed62cc9a4046e866c2a8c9ee8858
Surovi_M <59788708+Surovi3001@users.noreply.github.com>
2024-02-15 00:08:38 +0530

    UK_Accident.sql
    THIS FILE CONTAINS VARIOUS SQL QUERIES THAT HELPED ANALYSE THE DATA

--- UK_Accident.sql
+++ UK_Accident.sql
@@ -1,1 +1,19 @@
-https://public.tableau.com/views/CalculatedFields_17073228965260/Days?:language=en-US&:display_count=n&:origin=viz_share_link
+/* Questions with thier respective SQL Query */
+
+/* What is the number of casualties on each day of the week? */
+
+SELECT SUM(No_of_casualties), Day_of_week 
+FROM UK_Accident; 
+
+/* On each day of the week, what is the maximum and minimum speed limit on the roads where the accidents
+happened? */
+
+SELECT day_of_week, max(speed_limit) AS "Maximum Speed", min(speed_limit) AS "Minimum Speed"
+FROM uk_accident 
+GROUP BY day_of_week;
+
+/* What is the importance of Light and Weather conditions in predicting accident severity? */
+
+SELECT light_conditions, weather_conditions, COUNT(Accident_Severity)
+FROM uk_accident
+GROUP BY light_conditions, weather_conditions;
